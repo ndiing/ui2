0e5aea406c922978b523b03c94007729041de79e
Ridho Prasetya <ndiing.inc@gmail.com>
2024-04-24 04:17:04 +0700

    update slider

--- src/dev/slider/slider.js
+++ src/dev/slider/slider.js
@@ -6,23 +6,34 @@ class DevSlider extends MDElement {
     render() {
         // prettier-ignore
         return html`
-            <div class="md-layout__grid">
-                <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4">
-                    <br><br><br><md-slider name="centered"></md-slider>
+            <md-form
+                @onFormNativeSubmit="${this.handleFormNativeSubmit}"
+            >
+
+                <div class="md-layout__grid">
+                    <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4">
+                        <br><br><br><md-slider ui="centered" name="centered"></md-slider>
+                    </div>
+                    <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4">
+                        <br><br><br><md-slider ui="continuous" name="continuous"></md-slider>
+                    </div>
+                    <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4">
+                        <br><br><br><md-slider ui="discrete" name="discrete" step="10"></md-slider>
+                    </div>
+                    <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4">
+                        <br><br><br><md-slider ui="range-selection" name="range-selection" value='[25,75]' defaultValue='[25,75]'></md-slider>
+                    </div>
+                    <div class="md-layout__column--expanded12 md-layout__column--medium4 md-layout__column--compact4">
+                        <md-button type="reset" label="reset"></md-button>
+                        <md-button type="submit" label="submit"></md-button>
+                    </div>
                 </div>
-                <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4">
-                    <br><br><br><md-slider name="continuous"></md-slider>
-                </div>
-                <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4">
-                    <br><br><br><md-slider name="discrete"></md-slider>
-                </div>
-                <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4">
-                    <br><br><br><md-slider name="range-selection"></md-slider>
-                </div>
-                <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4"></div>
-                <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4"></div>
-            </div>
+            </md-form>
         `;
+    }
+    handleFormNativeSubmit(event){
+        const formData=(new FormData(event.detail.target))
+        console.log(Object.fromEntries(formData.entries()))
     }
 }
 
--- src/lib/slider/slider.js
+++ src/lib/slider/slider.js
@@ -3,36 +3,29 @@ import { html, nothing } from "lit";
 import { msg } from "@lit/localize";
 import { ifDefined } from "lit/directives/if-defined.js";
 
+function converter(value) {
+    value = JSON.parse(value);
+    return [].concat(value).map(Number);
+}
+
 class MDSlider extends MDElement {
     static get properties() {
         return {
-            type: { type: String },
             name: { type: String },
             required: { type: Boolean },
             readOnly: { type: Boolean },
-            value: { type: Number },
-            defaultValue: { type: Number },
+            value: { converter },
+            defaultValue: { converter },
             min: { type: Number },
             max: { type: Number },
             step: { type: Number },
+            ui: { type: String },
+            list: { type: Array },
         };
     }
 
     constructor() {
         super();
-
-        // this.type = "range";
-
-        this.min = 0;
-        this.max = 100;
-        this.value = 50;
-        this.defaultValue = this.value;
-        this.step=1
-        this.list=[
-            {value:this.min},
-            {value:this.value},
-            {value:this.max},
-        ]
     }
 
     render() {
@@ -40,23 +33,36 @@ class MDSlider extends MDElement {
         return html`
             <input 
                 class="md-slider__native"
-                .type="${'range'}"
+                type="hidden"
                 .name="${ifDefined(this.name)}"
-                .required="${ifDefined(this.required)}"
-                .readOnly="${ifDefined(this.readOnly)}"
                 .value="${ifDefined(this.value)}"
-                .defaultValue="${ifDefined(this.defaultValue)}"
-                .min="${ifDefined(this.min)}"
-                .max="${ifDefined(this.max)}"
-                .step="${ifDefined(this.step)}"
-                @focus="${this.handleSliderNativeFocus}"
-                @blur="${this.handleSliderNativeBlur}"
-                @input="${this.handleSliderNativeInput}"
-                @invalid="${this.handleSliderNativeInvalid}"
-                @reset="${this.handleSliderNativeReset}"
             >
-            <output class="md-slider__label">${this.value}</output>
-            <div>${this.list.map(item=>html`<span ?selected="${this.value>=item.value}"></span>`)}</div>
+            ${this.value?.map((value,index)=>html`
+                <div class="md-slider__container md-slider__container${index+1}">
+                    <input 
+                        class="md-slider__native md-slider__native${index+1}"
+                        type="range"
+                        .required="${ifDefined(this.required)}"
+                        .readOnly="${ifDefined(this.readOnly)}"
+                        .value="${ifDefined(this.value[index])}"
+                        .defaultValue="${ifDefined(this.defaultValue[index])}"
+                        .min="${ifDefined(this.min)}"
+                        .max="${ifDefined(this.max)}"
+                        .step="${ifDefined(this.step)}"
+                        @focus="${this.handleSliderNativeFocus}"
+                        @blur="${this.handleSliderNativeBlur}"
+                        @input="${this.handleSliderNativeInput}"
+                        @invalid="${this.handleSliderNativeInvalid}"
+                        @reset="${this.handleSliderNativeReset}"
+                    >
+                    <output class="md-slider__label">${this.value[index]}</output>
+                </div>
+            `)}
+            <div class="md-slider__list">
+                ${this.list?.map(item=>html`
+                    <span class="md-slider__item" ?selected="${this.value?.[0]>item}"></span>
+                `)}
+            </div>
         `
     }
 
@@ -71,19 +77,49 @@ class MDSlider extends MDElement {
     }
 
     firstUpdated() {
-
-        // fix invalid thumb position
-        // when using large number
-        this.sliderNative.value=this.value
-        this.sliderNative.defaultValue=this.defaultValue
+        
+        if(this.ui === 'centered'){
+            this.min = this.min?? -100;
+            this.max = this.max?? 100;
+            this.step = this.step?? 1;
+            this.value = this.value?? [this.getHalf(this.min,this.max)];
+            this.defaultValue = this.defaultValue?? this.value
+            this.list = [this.min, this.getHalf(this.min,this.max), this.max];
+        }
+        if(this.ui === 'continuous'){
+            this.min = this.min?? 0;
+            this.max = this.max?? 100;
+            this.step = this.step?? 1;
+            this.value = this.value?? [this.getHalf(this.min,this.max)];
+            this.defaultValue = this.defaultValue?? this.value
+            this.list = [this.min, this.getHalf(this.min,this.max), this.max];
+        }
+        if (this.ui === "discrete") {
+            this.min = this.min?? 0;
+            this.max = this.max?? 100;
+            this.step = this.step?? 10;
+            this.value = this.value?? [this.getHalf(this.min,this.max)];
+            this.defaultValue = this.defaultValue?? this.value
+            this.list = Array.from({ length: this.step + 1 }, (value, index) => {
+                return (this.max / this.step) * index;
+            });
+        }
+        if(this.ui === 'range-selection'){
+            this.min = this.min?? 0;
+            this.max = this.max?? 100;
+            this.step = this.step?? 1;
+            this.value = this.value?? [25,75];
+            this.defaultValue = this.defaultValue?? this.value
+            this.list = [this.min, this.getHalf(this.min,this.max) / 2, this.max];
+        }
 
         this.updateStyle();
     }
 
     updated(changedProperties) {
-        if (changedProperties.has("type")) {
-            if (this.type) {
-                this.classList.add("md-slider--" + this.type);
+        if (changedProperties.has("ui")) {
+            if (this.ui) {
+                this.classList.add("md-slider--" + this.ui);
             } else {
                 this.classList.remove("md-slider--centered");
                 this.classList.remove("md-slider--continuous");
@@ -93,12 +129,23 @@ class MDSlider extends MDElement {
         }
     }
 
-    get sliderNative() {
-        return this.querySelector(".md-slider__native");
+    get sliderNative1() {
+        return this.querySelector(".md-slider__native.md-slider__native1");
     }
-    get sliderLabel() {
-        return this.querySelector(".md-slider__label");
+    get sliderNative2() {
+        return this.querySelector(".md-slider__native.md-slider__native2");
     }
+
+    getHalf(min, max) {
+        return (min+max) / 2;
+    }
+    getDecimal(value, min, max) {
+        return (value - min) / (max - min);
+    }
+    getPercentage(value, min, max) {
+        return ((value - min) / (max - min)) * 100;
+    }
+
     handleSliderNativeFocus(event) {
         this.emit("onSliderNativeFocus", event);
     }
@@ -106,25 +153,19 @@ class MDSlider extends MDElement {
         this.emit("onSliderNativeBlur", event);
     }
     handleSliderNativeInput(event) {
-        this.value = this.sliderNative.value;
+        this.value = [this.sliderNative1.value, this.sliderNative2?.value].filter(Boolean);
+
         this.updateStyle();
 
         this.emit("onSliderNativeInput", event);
     }
-
-    getMiddle(min,max){
-        return (min+max)/2
-    }
-    getDecimal(value,min,max){
-        return (value - min) / (max - min) 
-    }
-    getPercentage(value,min,max){
-        return (value - min) / (max - min) * 100
-    }
-
     updateStyle() {
-        this.style.setProperty("--md-slider-decimal", this.getDecimal(this.value,this.min,this.max));
-        this.style.setProperty("--md-slider-percentage", this.getPercentage(this.value,this.min,this.max)+'%');
+        this.value?.forEach((value, index) => {
+            const decimal = this.getDecimal(this.value[index], this.min, this.max);
+            const percentage = this.getPercentage(this.value[index], this.min, this.max);
+            this.style.setProperty("--md-slider-decimal" + (index + 1), decimal);
+            this.style.setProperty("--md-slider-percentage" + (index + 1), percentage + "%");
+        });
     }
 
     handleSliderNativeInvalid(event) {
@@ -133,6 +174,8 @@ class MDSlider extends MDElement {
         this.emit("onSliderNativeInvalid", event);
     }
     handleSliderNativeReset(event) {
+        this.value = this.defaultValue
+
         this.emit("onSliderNativeReset", event);
     }
 }
