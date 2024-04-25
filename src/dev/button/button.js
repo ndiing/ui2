75b42c9d50acffa2b6c8c953148d2528337c7891
Ridho Prasetya <ndiing.inc@gmail.com>
2024-04-26 01:49:27 +0700

    adding button example ui

--- src/dev/button/button.js
+++ src/dev/button/button.js
@@ -11,6 +11,9 @@ class DevButton extends MDElement {
                     <md-button label="label"></md-button>
                 </div>
                 <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4">
+                    <md-button ui="outlined" label="label"></md-button>
+                </div>
+                <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4">
                     <md-button ui="filled-tonal" label="label"></md-button>
                 </div>
                 <div class="md-layout__column--expanded4 md-layout__column--medium4 md-layout__column--compact4">
