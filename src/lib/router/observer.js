bb461f6bf9bd9331a54cec38566b7e882e0851bd
Ridho Prasetya <ndiing.inc@gmail.com>
2024-04-27 02:41:12 +0700

    add observer

--- src/lib/router/observer.js
+++ src/lib/router/observer.js
@@ -1,12 +1,12 @@
-class Observer{
-    constructor(list=[],callback=() => {}){
-        this.list=list
-        this.callback=callback
-        this.handleChange=this.handleChange.bind(this)
+class Observer {
+    constructor(list = [], callback = () => {}) {
+        this.list = list;
+        this.callback = callback;
+        this.handleChange = this.handleChange.bind(this);
         this.handleChange();
     }
 
-    handleChange(){
+    handleChange() {
         this.mql?.removeEventListener("change", this.handleChange);
         for (this.item of this.list) {
             this.mql = window.matchMedia(this.item.value);
@@ -19,36 +19,39 @@ class Observer{
     }
 }
 
-function emit(type,detail){
-    const event = new CustomEvent(type,{
-        bubbles:true,
-        cancelable:true,
-        detail
-    })
-    window.dispatchEvent(event)
+function emit(type, detail) {
+    const event = new CustomEvent(type, {
+        bubbles: true,
+        cancelable: true,
+        detail,
+    });
+    window.dispatchEvent(event);
 }
 
-let layout
-let color
+let layout;
+let color;
 
-new Observer([
-    { name: "compact", value: "(max-width: 599px)" },
-    { name: "medium", value: "(min-width: 600px) and (max-width: 839px)" },
-    { name: "expanded", value: "(min-width: 840px)" },
-],(item) => {
-    layout=item
-    emit('onLayoutChange',item)
-})
+new Observer(
+    [
+        { name: "compact", value: "(max-width: 599px)" },
+        { name: "medium", value: "(min-width: 600px) and (max-width: 839px)" },
+        { name: "expanded", value: "(min-width: 840px)" },
+    ],
+    (item) => {
+        layout = item;
+        emit("onLayoutChange", item);
+    },
+);
 
-new Observer([
-    { name: "light", value: "(prefers-color-scheme: light)" },
-    { name: "dark", value: "(prefers-color-scheme: dark)" },
-],(item) => {
-    color=item
-    emit('onColorChange',item)
-})
+new Observer(
+    [
+        { name: "light", value: "(prefers-color-scheme: light)" },
+        { name: "dark", value: "(prefers-color-scheme: dark)" },
+    ],
+    (item) => {
+        color = item;
+        emit("onColorChange", item);
+    },
+);
 
-export {
-    layout,
-    color,
-}
+export { layout, color };
