759cc63b3ac6ada2de741802de62f379a7c583a1
Ridho Prasetya <ndiing.inc@gmail.com>
2024-05-05 00:39:29 +0700

    save

--- src/dev/side-sheet/side-sheet.js
+++ src/dev/side-sheet/side-sheet.js
@@ -9,9 +9,9 @@ class DevSideSheet extends MDElement {
             <div class="md-layout--border md-layout--fit">
                 <div class="md-layout__item--east md-layout--fit">
                     <md-side-sheet 
-                        id="sideSheet"
-                        @onSideSheetActionClick="${this.handleClick}"
-                        @onSideSheetButtonClick="${this.handleClick}"
+                        id="sideSheet1"
+                        @onSideSheetActionClick="${this.handleClick1}"
+                        @onSideSheetButtonClick="${this.handleClick1}"
                         leadingActions='["home"]' label="Label" trailingActions='["close"]' buttons='[{"label":"save","ui":"filled"}]'
                     >body</md-side-sheet>
                     
@@ -24,16 +24,16 @@ class DevSideSheet extends MDElement {
                     >body</md-side-sheet>
                     
                 </div>
-                <div class="md-layout__item--center">
-                    <md-button id="button" label="side sheet" @click="${this.handleClick}"></md-button>
-                    <md-button id="button" label="side sheet modal" @click="${this.handleClick2}"></md-button>
+                <div class="md-layout__item--center" style="padding:24px;">
+                    <md-button ui="filled-tonal" label="Standard side sheet" @click="${this.handleClick1}"></md-button>
+                    <md-button ui="filled-tonal" label="Modal side sheet" @click="${this.handleClick2}"></md-button>
                 </div>
             </div>
         `;
     }
-    handleClick(event) {
-        if (sideSheet.open) sideSheet.close();
-        else sideSheet.show();
+    handleClick1(event) {
+        if (sideSheet1.open) sideSheet1.close();
+        else sideSheet1.show();
     }
     handleClick2(event) {
         if (sideSheet2.open) sideSheet2.close();
