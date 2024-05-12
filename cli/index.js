225bc0cd781745606922d9e5f3eb222267d1bb21
Ridho Prasetya <ndiing.inc@gmail.com>
2024-05-13 04:57:46 +0700

    update cli

--- cli/index.js
+++ cli/index.js
@@ -74,7 +74,7 @@ const rl = readline.createInterface({
 
 const commands = ["create component <name>", "delete component <name>", "create controller <name>", "delete controller <name>", "create dev <name>", "delete dev <name>", "create app <name>", "delete app <name>"];
 
-function printWelcomeMessage() {
+function print() {
     console.log("Welcome to My CLI!");
     console.log("Available commands:");
     commands.forEach((command) => console.log(command));
@@ -126,13 +126,72 @@ function processCommand(answer) {
 }
 
 function createComponent(name) {
-    console.log(`Creating component: ${name}`);
-    // Implement your logic to create a component
+    var fileJs = `./src/lib/${name}/${name}.js`;
+    var fileScss = `./src/lib/${name}/${name}.scss`;
+
+    var exist = fs.existsSync(fileJs);
+    if (exist) {
+        console.log(`Component: ${name} already exists`);
+    } else {
+        console.log(`Creating component: ${name}`);
+
+        var templateJs = read("./cli/component/template.js");
+        templateJs = templateJs.replaceAll("template", name).replaceAll("Template", toPascalCase(name));
+        write(fileJs, templateJs);
+
+        var templateScss = read("./cli/component/template.scss");
+        templateScss = templateScss.replaceAll("template", name).replaceAll("Template", toPascalCase(name));
+        write(fileScss, templateScss);
+
+        var indexJs = read("./src/lib/index.js");
+        var listJs = indexJs.split(/\r\n/).filter(Boolean);
+        var js = `import "./${name}/${name}.js";`;
+        listJs.push(js);
+        indexJs = listJs.join("\r\n");
+        write("./src/lib/index.js", indexJs);
+
+        var indexScss = read("./src/lib/index.scss");
+        var listScss = indexScss.split(/\r\n/).filter(Boolean);
+        var scss = `@import "./${name}/${name}.scss";`;
+        listScss.push(scss);
+        indexScss = listScss.join("\r\n");
+        write("./src/lib/index.scss", indexScss);
+    }
 }
 
 function deleteComponent(name) {
-    console.log(`Deleting component: ${name}`);
-    // Implement your logic to delete a component
+    var fileJs = `./src/lib/${name}/${name}.js`;
+    var fileScss = `./src/lib/${name}/${name}.scss`;
+    var dir = path.dirname(fileJs);
+
+    var exist = fs.existsSync(fileJs);
+    if (!exist) {
+        console.log(`Component: ${Name} None`);
+    } else {
+        console.log(`Deleting component: ${name}`);
+
+        removeDir(dir);
+
+        var indexJs = read("./src/lib/index.js");
+        var listJs = indexJs.split(/\r\n/).filter(Boolean);
+        var js = `import "./${name}/${name}.js";`;
+        var iJs = listJs.indexOf(js);
+        if (iJs >= -1) {
+            listJs.splice(iJs, 1);
+        }
+        indexJs = listJs.join("\r\n");
+        write("./src/lib/index.js", indexJs);
+
+        var indexScss = read("./src/lib/index.scss");
+        var listScss = indexScss.split(/\r\n/).filter(Boolean);
+        var scss = `@import "./${name}/${name}.scss";`;
+        var iScss = listScss.indexOf(scss);
+        if (iScss >= -1) {
+            listScss.splice(iScss, 1);
+        }
+        indexScss = listScss.join("\r\n");
+        write("./src/lib/index.scss", indexScss);
+    }
 }
 
 function createController(name) {
@@ -169,12 +228,10 @@ function createController(name) {
     }
 }
 
-// createController("example");
-
 function deleteController(name) {
     var fileJs = `./src/lib/${name}/${name}.js`;
     var fileScss = `./src/lib/${name}/${name}.scss`;
-    var dir = path.dirname(fileJs)
+    var dir = path.dirname(fileJs);
 
     var exist = fs.existsSync(fileJs);
     if (!exist) {
@@ -182,47 +239,101 @@ function deleteController(name) {
     } else {
         console.log(`Deleting controller: ${name}`);
 
-        removeDir(dir)
+        removeDir(dir);
 
         var indexJs = read("./src/lib/index.js");
         var listJs = indexJs.split(/\r\n/).filter(Boolean);
         var js = `import "./${name}/${name}.js";`;
-        var indexJs = listJs.indexOf(js);
-        listJs.splice(indexJs,1)
+        var iJs = listJs.indexOf(js);
+        if (iJs >= -1) {
+            listJs.splice(iJs, 1);
+        }
         indexJs = listJs.join("\r\n");
         write("./src/lib/index.js", indexJs);
 
         var indexScss = read("./src/lib/index.scss");
         var listScss = indexScss.split(/\r\n/).filter(Boolean);
         var scss = `@import "./${name}/${name}.scss";`;
-        var indexScss = listScss.indexOf(scss);
-        listScsslistJs.splice(indexScss,1)
+        var iScss = listScss.indexOf(scss);
+        if (iScss >= -1) {
+            listScss.splice(iScss, 1);
+        }
         indexScss = listScss.join("\r\n");
         write("./src/lib/index.scss", indexScss);
     }
 }
 
 function createDev(name) {
-    console.log(`Creating development environment: ${name}`);
-    // Implement your logic to create a development environment
+    var fileJs = `./src/dev/${name}/${name}.js`;
+    var fileScss = `./src/dev/${name}/${name}.scss`;
+
+    var exist = fs.existsSync(fileJs);
+    if (exist) {
+        console.log(`Dev: ${name} already exists`);
+    } else {
+        console.log(`Creating dev: ${name}`);
+
+        var templateJs = read("./cli/dev/template.js");
+        templateJs = templateJs.replaceAll("template", name).replaceAll("Template", toPascalCase(name));
+        write(fileJs, templateJs);
+
+        var templateScss = read("./cli/dev/template.scss");
+        templateScss = templateScss.replaceAll("template", name).replaceAll("Template", toPascalCase(name));
+        write(fileScss, templateScss);
+    }
 }
 
 function deleteDev(name) {
-    console.log(`Deleting development environment: ${name}`);
-    // Implement your logic to delete a development environment
+    var fileJs = `./src/dev/${name}/${name}.js`;
+    var fileScss = `./src/dev/${name}/${name}.scss`;
+    var dir = path.dirname(fileJs);
+
+    var exist = fs.existsSync(fileJs);
+    if (!exist) {
+        console.log(`Dev: ${Name} None`);
+    } else {
+        console.log(`Deleting dev: ${name}`);
+
+        removeDir(dir);
+    }
 }
 
 function createApp(name) {
-    console.log(`Creating application: ${name}`);
-    // Implement your logic to create an application
+    var fileJs = `./src/app/${name}/${name}.js`;
+    var fileScss = `./src/app/${name}/${name}.scss`;
+
+    var exist = fs.existsSync(fileJs);
+    if (exist) {
+        console.log(`App: ${name} already exists`);
+    } else {
+        console.log(`Creating app: ${name}`);
+
+        var templateJs = read("./cli/app/template.js");
+        templateJs = templateJs.replaceAll("template", name).replaceAll("Template", toPascalCase(name));
+        write(fileJs, templateJs);
+
+        var templateScss = read("./cli/app/template.scss");
+        templateScss = templateScss.replaceAll("template", name).replaceAll("Template", toPascalCase(name));
+        write(fileScss, templateScss);
+    }
 }
 
 function deleteApp(name) {
-    console.log(`Deleting application: ${name}`);
-    // Implement your logic to delete an application
+    var fileJs = `./src/app/${name}/${name}.js`;
+    var fileScss = `./src/app/${name}/${name}.scss`;
+    var dir = path.dirname(fileJs);
+
+    var exist = fs.existsSync(fileJs);
+    if (!exist) {
+        console.log(`App: ${Name} None`);
+    } else {
+        console.log(`Deleting app: ${name}`);
+
+        removeDir(dir);
+    }
 }
 
-printWelcomeMessage();
+print();
 
 rl.question("Enter command: ", (answer) => {
     processCommand(answer);
--- src/lib/index.js
+++ src/lib/index.js
@@ -8,21 +8,17 @@ import "./gesture/gesture.js";
 import "./ripple/ripple.js";
 import "./popper/popper.js";
 import "./virtual-scroll/virtual-scroll.js";
-
 import "./localization/localization.js";
 import "./element/element.js";
-
 import "./icon/icon.js";
 import "./badge/badge.js";
 import "./divider/divider.js";
-
 import "./button/button.js";
 import "./segmented-button/segmented-button.js";
 import "./icon-button/icon-button.js";
 import "./fab/fab.js";
 import "./extended-fab/extended-fab.js";
 import "./chip/chip.js";
-
 import "./layout/layout.js";
 import "./pane/pane.js";
 import "./card/card.js";
@@ -33,7 +29,6 @@ import "./side-sheet/side-sheet.js";
 import "./snackbar/snackbar.js";
 import "./tooltip/tooltip.js";
 import "./progress-indicator/progress-indicator.js";
-
 import "./list/list.js";
 import "./navigation-bar/navigation-bar.js";
 import "./navigation-drawer/navigation-drawer.js";
@@ -42,22 +37,18 @@ import "./tab/tab.js";
 import "./top-app-bar/top-app-bar.js";
 import "./bottom-app-bar/bottom-app-bar.js";
 import "./menu/menu.js";
-
 import "./date-picker/date-picker.js";
 import "./time-picker/time-picker.js";
 import "./week-picker/week-picker.js";
 import "./month-picker/month-picker.js";
 import "./datetime-picker/datetime-picker.js";
 import "./color-picker/color-picker.js";
-
 import "./form/form.js";
-
 import "./email-field/email-field.js";
 import "./password-field/password-field.js";
 import "./text-field/text-field.js";
 import "./url-field/url-field.js";
 import "./tel-field/tel-field.js";
-
 import "./date-field/date-field.js";
 import "./datetime-field/datetime-field.js";
 import "./month-field/month-field.js";
@@ -65,7 +56,6 @@ import "./time-field/time-field.js";
 import "./week-field/week-field.js";
 import "./color-field/color-field.js";
 import "./file-field/file-field.js";
-
 import "./textarea-field/textarea-field.js";
 import "./select-field/select-field.js";
 import "./number-field/number-field.js";
@@ -74,6 +64,5 @@ import "./checkbox/checkbox.js";
 import "./radio-button/radio-button.js";
 import "./switch/switch.js";
 import "./slider/slider.js";
-
 import "./data-table/data-table.js";
 import "./pagination/pagination.js";
