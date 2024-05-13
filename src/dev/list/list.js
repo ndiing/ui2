27b7d4f92cafa25dc36c8e811a4bb1836b5d1093
Ridho Prasetya <ndiing.inc@gmail.com>
2024-05-13 12:15:10 +0700

    save

--- src/dev/list/list.js
+++ src/dev/list/list.js
@@ -9,169 +9,169 @@ class DevList extends MDElement {
         return html`
             <div class="md-layout--column" style="margin:24px;">
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {avatar:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {avatar:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text'},
                             {avatar:'https://placehold.co/600x400',label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {trailingCheckbox:{},avatar:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {trailingCheckbox:{},avatar:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text'},
                             {trailingCheckbox:{},avatar:'https://placehold.co/600x400',label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
                 </div>
 
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {image:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {image:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text'},
                             {image:'https://placehold.co/600x400',label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {trailingCheckbox:{},image:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {trailingCheckbox:{},image:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text'},
                             {trailingCheckbox:{},image:'https://placehold.co/600x400',label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
                 </div>
 
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {video:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {video:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text'},
                             {video:'https://placehold.co/600x400',label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {trailingCheckbox:{},video:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {trailingCheckbox:{},video:'https://placehold.co/600x400',label:'Headline',labelSecondary:'Supporting text'},
                             {trailingCheckbox:{},video:'https://placehold.co/600x400',label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
                 </div>
 
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {icon:'image',label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {icon:'image',label:'Headline',labelSecondary:'Supporting text'},
                             {icon:'image',label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {trailingCheckbox:{},icon:'image',label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {trailingCheckbox:{},icon:'image',label:'Headline',labelSecondary:'Supporting text'},
                             {trailingCheckbox:{},icon:'image',label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
                 </div>
 
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {label:'Headline',labelSecondary:'Supporting text'},
                             {label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {trailingCheckbox:{},label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {trailingCheckbox:{},label:'Headline',labelSecondary:'Supporting text'},
                             {trailingCheckbox:{},label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
                 </div>
 
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {leadingCheckbox:{},label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {leadingCheckbox:{},label:'Headline',labelSecondary:'Supporting text'},
                             {leadingCheckbox:{},label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {leadingCheckbox:{},text:'100+',label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {leadingCheckbox:{},text:'100+',label:'Headline',labelSecondary:'Supporting text'},
                             {leadingCheckbox:{},text:'100+',label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
                 </div>
 
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {leadingRadioButton:{},label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {leadingRadioButton:{},label:'Headline',labelSecondary:'Supporting text'},
                             {leadingRadioButton:{},label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {leadingRadioButton:{},text:'100+',label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {leadingRadioButton:{},text:'100+',label:'Headline',labelSecondary:'Supporting text'},
                             {leadingRadioButton:{},text:'100+',label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
                 </div>
 
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {trailingSwitch:{},label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {trailingSwitch:{},label:'Headline',labelSecondary:'Supporting text'},
                             {trailingSwitch:{},label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
-                    <md-example
+                    <md-list
                         .list="${[
                             {trailingSwitch:{},icon:'image',label:'Headline',labelSecondary:'Supporting text that is long enough to fill up multiple lines'},
                             {trailingSwitch:{},icon:'image',label:'Headline',labelSecondary:'Supporting text'},
                             {trailingSwitch:{},icon:'image',label:'Headline'},
                         ]}"
-                    ></md-example>
+                    ></md-list>
                 </div>
                 <div class="md-layout__item--expanded4 md-layout__item--medium4 md-layout__item--compact4">
                 </div>
