fb5708a985d4284152a6c794546964100669e65e
Ridho Prasetya <ndiing.inc@gmail.com>
2024-05-15 06:12:06 +0700

    save

--- src/dev/layout/layout.js
+++ src/dev/layout/layout.js
@@ -6,20 +6,20 @@ class DevLayout extends MDElement {
     render() {
         // prettier-ignore
         return html`
-            <div class="md-layout--border md-layout--fit">
-                <div class="md-layout__item md-layout__item--north" style="height:80px; margin-top:-80px;">
+            <div class="md-layout-border md-layout-fit">
+                <div class="md-layout-border__item md-layout-border__item--north" style="height:80px; margin-top:-80px;">
                     north
                 </div>
-                <div class="md-layout__item md-layout__item--east" style="width:360px; margin-right:-360px;">
+                <div class="md-layout-border__item md-layout-border__item--east" style="width:360px; margin-right:-360px;">
                     east
                 </div>
-                <div class="md-layout__item md-layout__item--south" style="height:80px; margin-bottom:-80px;">
+                <div class="md-layout-border__item md-layout-border__item--south" style="height:80px; margin-bottom:-80px;">
                     south
                 </div>
-                <div class="md-layout__item md-layout__item--west" style="width:360px; margin-left:-360px;">
+                <div class="md-layout-border__item md-layout-border__item--west" style="width:360px; margin-left:-360px;">
                     west
                 </div>
-                <div class="md-layout__item md-layout__item--center">
+                <div class="md-layout-border__item md-layout-border__item--center">
                     center
                 </div>
             </div>
