582d6a5d44d6d6a02f8f2a26fde64c05c8178dd1
Ridho Prasetya <ndiing.inc@gmail.com>
2024-05-15 07:52:37 +0700

    save

--- src/lib/card/card.js
+++ src/lib/card/card.js
@@ -1,50 +1,60 @@
+import { ifDefined } from "lit/directives/if-defined.js";
 import { MDElement } from "../element/element";
 import { html, nothing } from "lit";
+
 class MDCard extends MDElement {
     static get properties() {
         return {
             leadingActions: { type: Array },
             label: { type: String },
+            labelSecondary: { type: String },
             trailingActions: { type: Array },
             buttons: { type: Array },
+
             ui: { type: String },
+
+            open: { type: Boolean },
         };
     }
 
     constructor() {
         super();
+
         this.body = Array.from(this.childNodes);
     }
 
     render() {
         // prettier-ignore
         return html`
-            ${this.leadingActions?.length||this.label||this.trailingActions?.length?html`
+            ${this.leadingActions?.length||this.label||this.labelSecondary||this.trailingActions?.length?html`
                 <div class="md-card__header">
                     ${this.leadingActions?.length?html`
                         <div class="md-card__actions">
-                            ${this.leadingActions?.map(action=>html`
+                            ${this.leadingActions.map(action => html`
                                 <md-icon-button 
                                     class="md-card__action"
-                                    .icon="${action?.icon??action}"
-                                    .type="${action?.type}"
-                                    .ui="${action?.ui}"
+                                    .icon="${ifDefined(action.icon??action)}"
+                                    .type="${ifDefined(action.type)}"
+                                    .ui="${ifDefined(action.ui)}"
                                     @click="${this.handleCardActionClick}"
                                 ></md-icon-button>
                             `)}
                         </div>
                     `:nothing}
-                    ${this.label?html`
-                        <div class="md-card__label">${this.label}</div>
+                    ${this.label||this.labelSecondary?html`
+                        <div class="md-card__label">
+                            ${this.label?html`<div class="md-card__label-primary">${this.label}</div>`:nothing}
+                            ${this.labelSecondary?html`<div class="md-card__label-secondary">${this.labelSecondary}</div>`:nothing}
+                        </div>
                     `:nothing}
                     ${this.trailingActions?.length?html`
                         <div class="md-card__actions">
-                            ${this.trailingActions?.map(action=>html`
+                            ${this.trailingActions.map(action => html`
                                 <md-icon-button 
                                     class="md-card__action"
-                                    .icon="${action?.icon??action}"
-                                    .type="${action?.type}"
-                                    .ui="${action?.ui}"
+                                    .icon="${ifDefined(action.icon??action)}"
+                                    .type="${ifDefined(action.type)}"
+                                    .ui="${ifDefined(action.ui)}"
                                     @click="${this.handleCardActionClick}"
                                 ></md-icon-button>
                             `)}
@@ -52,49 +62,69 @@ class MDCard extends MDElement {
                     `:nothing}
                 </div>
             `:nothing}
-            <div class="md-card__body">
-                <div class="md-card__inner">${this.body}</div>
-                ${this.buttons?.length?html`
-                    <div class="md-card__footer">
-                        ${this.buttons?.map(button=>html`
-                            <md-button 
-                                class="md-card__button"
-                                .label="${button?.label??button}"
-                                .type="${button?.type}"
-                                .ui="${button?.ui}"
-                                .selected="${button?.selected}"
-                                @click="${this.handleCardButtonClick}"
-                            ></md-button>
-                        `)}
-                    </div>
-                `:nothing}
-            </div>
-        `
+            ${this.body?.length||this.buttons?.length?html`
+                <div class="md-card__body">
+                    ${this.body?.length?html`<div class="md-card__inner">${this.body}</div>`:nothing}
+                    ${this.buttons?.length?html`
+                        <div class="md-card__footer">
+                            ${this.buttons.map(action => html`
+                                <md-button 
+                                    class="md-card__button"
+                                    .icon="${ifDefined(action.icon)}"
+                                    .label="${ifDefined(action.label??action)}"
+                                    .type="${ifDefined(action.type)}"
+                                    .ui="${ifDefined(action.ui)}"
+                                    .selected="${ifDefined(action.selected)}"
+                                    @click="${this.handleCardButtonClick}"
+                                ></md-button>
+                            `)}
+                        </div>
+                    `:nothing}
+                </div>
+            `:nothing}
+        `;
     }
 
-    async connectedCallback() {
+    connectedCallback() {
         super.connectedCallback();
-        await this.updateComplete;
+
         this.classList.add("md-card");
+
     }
 
     disconnectedCallback() {
         super.disconnectedCallback();
         this.classList.remove("md-card");
+
     }
+
+    firstUpdated(changedProperties) {}
 
     updated(changedProperties) {
         if (changedProperties.has("ui")) {
-            ["elevated", "filled", "outlined"].forEach((ui) => {
+            [
+                //
+                "elevated",
+                "outlined",
+                "filled",
+            ].forEach((ui) => {
                 this.classList.remove("md-card--" + ui);
             });
-
             if (this.ui) {
                 this.ui.split(" ").forEach((ui) => {
                     this.classList.add("md-card--" + ui);
                 });
             }
         }
+        
+    }
+
+    show() {
+        this.open = true;
+    }
+
+    close() {
+        this.open = false;
     }
 
     handleCardActionClick(event) {
@@ -104,6 +134,9 @@ class MDCard extends MDElement {
     handleCardButtonClick(event) {
         this.emit("onCardButtonClick", event);
     }
+
 }
+
 customElements.define("md-card", MDCard);
+
 export { MDCard };
