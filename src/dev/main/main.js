15f6f7465bdad5b7c91698de9e59384271a4f193
Ridho Prasetya <ndiing.inc@gmail.com>
2024-05-16 08:15:59 +0700

    save

--- src/dev/main/main.js
+++ src/dev/main/main.js
@@ -8,26 +8,26 @@ class DevMain extends MDElement {
     constructor() {
         super();
         this.list = [
-            // {
-            //     label: "app-bar",
-            //     children: [
-            //         { label: "bottom-app-bar", routerLink: "/bottom-app-bar" },
-            //         { label: "top-app-bar", routerLink: "/top-app-bar" },
-            //     ],
-            // },
+            {
+                label: "app-bar",
+                children: [
+                    { label: "bottom-app-bar", routerLink: "/bottom-app-bar" },
+                    { label: "top-app-bar", routerLink: "/top-app-bar" },
+                ],
+            },
 
             { label: "badge", routerLink: "/badge" },
 
-            // {
-            //     label: "button",
-            //     children: [
-            //         { label: "button", routerLink: "/button" },
-            //         { label: "fab", routerLink: "/fab" },
-            //         { label: "extended-fab", routerLink: "/extended-fab" },
-            //         { label: "icon-button", routerLink: "/icon-button" },
-            //         { label: "segmented-button", routerLink: "/segmented-button" },
-            //     ],
-            // },
+            {
+                label: "button",
+                children: [
+                    { label: "button", routerLink: "/button" },
+                    { label: "fab", routerLink: "/fab" },
+                    { label: "extended-fab", routerLink: "/extended-fab" },
+                    { label: "icon-button", routerLink: "/icon-button" },
+                    { label: "segmented-button", routerLink: "/segmented-button" },
+                ],
+            },
 
             { label: "card", routerLink: "/card" },
             { label: "carousel", routerLink: "/carousel" },
@@ -39,25 +39,25 @@ class DevMain extends MDElement {
             { label: "list", routerLink: "/list" },
             { label: "menu", routerLink: "/menu" },
 
-            // {
-            //     label: "navigation",
-            //     children: [
-            //         { label: "navigation-bar", routerLink: "/navigation-bar" },
-            //         { label: "navigation-drawer", routerLink: "/navigation-drawer" },
-            //         { label: "navigation-rail", routerLink: "/navigation-rail" },
-            //     ],
-            // },
+            {
+                label: "navigation",
+                children: [
+                    { label: "navigation-bar", routerLink: "/navigation-bar" },
+                    { label: "navigation-drawer", routerLink: "/navigation-drawer" },
+                    { label: "navigation-rail", routerLink: "/navigation-rail" },
+                ],
+            },
 
             { label: "progress-indicator", routerLink: "/progress-indicator" },
             { label: "radio-button", routerLink: "/radio-button" },
 
-            // {
-            //     label: "sheet",
-            //     children: [
-            //         { label: "bottom-sheet", routerLink: "/bottom-sheet" },
-            //         { label: "side-sheet", routerLink: "/side-sheet" },
-            //     ],
-            // },
+            {
+                label: "sheet",
+                children: [
+                    { label: "bottom-sheet", routerLink: "/bottom-sheet" },
+                    { label: "side-sheet", routerLink: "/side-sheet" },
+                ],
+            },
 
             { label: "slider", routerLink: "/slider" },
             { label: "snackbar", routerLink: "/snackbar" },
@@ -67,51 +67,51 @@ class DevMain extends MDElement {
             { label: "time-picker", routerLink: "/time-picker" },
             { label: "tooltip", routerLink: "/tooltip" },
 
-            // {
-            //     label: "uncategorized",
-            //     children: [
-            //         { label: "color-field", routerLink: "/color-field" },
-            //         { label: "color-picker", routerLink: "/color-picker" },
-            //         { label: "data-table", routerLink: "/data-table" },
-            //         { label: "date-field", routerLink: "/date-field" },
-            //         { label: "datetime-field", routerLink: "/datetime-field" },
-            //         { label: "datetime-picker", routerLink: "/datetime-picker" },
-            //         { label: "element", routerLink: "/element" },
-            //         { label: "email-field", routerLink: "/email-field" },
-            //         { label: "emoji", routerLink: "/emoji" },
-            //         { label: "file-field", routerLink: "/file-field" },
-            //         { label: "form", routerLink: "/form" },
-            //         { label: "gesture", routerLink: "/gesture" },
-            //         { label: "icon", routerLink: "/icon" },
-            //         { label: "image", routerLink: "/image" },
-            //         { label: "layout", routerLink: "/layout" },
-            //         { label: "localization", routerLink: "/localization" },
-            //         { label: "marker", routerLink: "/marker" },
-            //         { label: "month-field", routerLink: "/month-field" },
-            //         { label: "month-picker", routerLink: "/month-picker" },
-            //         { label: "number-field", routerLink: "/number-field" },
-            //         { label: "observer", routerLink: "/observer" },
-            //         { label: "pagination", routerLink: "/pagination" },
-            //         { label: "pane", routerLink: "/pane" },
-            //         { label: "password-field", routerLink: "/password-field" },
-            //         { label: "polyfill", routerLink: "/polyfill" },
-            //         { label: "popper", routerLink: "/popper" },
-            //         { label: "progress", routerLink: "/progress" },
-            //         { label: "ripple", routerLink: "/ripple" },
-            //         { label: "router", routerLink: "/router" },
-            //         { label: "search-field", routerLink: "/search-field" },
-            //         { label: "select-field", routerLink: "/select-field" },
-            //         { label: "store", routerLink: "/store" },
-            //         { label: "tel-field", routerLink: "/tel-field" },
-            //         { label: "textarea-field", routerLink: "/textarea-field" },
-            //         { label: "time-field", routerLink: "/time-field" },
-            //         { label: "url-field", routerLink: "/url-field" },
-            //         { label: "virtual-scroll", routerLink: "/virtual-scroll" },
-            //         { label: "week-field", routerLink: "/week-field" },
-            //         { label: "week-picker", routerLink: "/week-picker" },
-            //         { label: "nested-list", routerLink: "/nested-list" },
-            //     ],
-            // },
+            {
+                label: "uncategorized",
+                children: [
+                    { label: "color-field", routerLink: "/color-field" },
+                    { label: "color-picker", routerLink: "/color-picker" },
+                    { label: "data-table", routerLink: "/data-table" },
+                    { label: "date-field", routerLink: "/date-field" },
+                    { label: "datetime-field", routerLink: "/datetime-field" },
+                    { label: "datetime-picker", routerLink: "/datetime-picker" },
+                    { label: "element", routerLink: "/element" },
+                    { label: "email-field", routerLink: "/email-field" },
+                    { label: "emoji", routerLink: "/emoji" },
+                    { label: "file-field", routerLink: "/file-field" },
+                    { label: "form", routerLink: "/form" },
+                    { label: "gesture", routerLink: "/gesture" },
+                    { label: "icon", routerLink: "/icon" },
+                    { label: "image", routerLink: "/image" },
+                    { label: "layout", routerLink: "/layout" },
+                    { label: "localization", routerLink: "/localization" },
+                    { label: "marker", routerLink: "/marker" },
+                    { label: "month-field", routerLink: "/month-field" },
+                    { label: "month-picker", routerLink: "/month-picker" },
+                    { label: "number-field", routerLink: "/number-field" },
+                    { label: "observer", routerLink: "/observer" },
+                    { label: "pagination", routerLink: "/pagination" },
+                    { label: "pane", routerLink: "/pane" },
+                    { label: "password-field", routerLink: "/password-field" },
+                    { label: "polyfill", routerLink: "/polyfill" },
+                    { label: "popper", routerLink: "/popper" },
+                    { label: "progress", routerLink: "/progress" },
+                    { label: "ripple", routerLink: "/ripple" },
+                    { label: "router", routerLink: "/router" },
+                    { label: "search-field", routerLink: "/search-field" },
+                    { label: "select-field", routerLink: "/select-field" },
+                    { label: "store", routerLink: "/store" },
+                    { label: "tel-field", routerLink: "/tel-field" },
+                    { label: "textarea-field", routerLink: "/textarea-field" },
+                    { label: "time-field", routerLink: "/time-field" },
+                    { label: "url-field", routerLink: "/url-field" },
+                    { label: "virtual-scroll", routerLink: "/virtual-scroll" },
+                    { label: "week-field", routerLink: "/week-field" },
+                    { label: "week-picker", routerLink: "/week-picker" },
+                    { label: "nested-list", routerLink: "/nested-list" },
+                ],
+            },
         ];
 
         const setSelected = (list) => {
