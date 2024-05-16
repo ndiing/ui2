ad2c044415e3989313e8ef12a107651ffbd029a8
Ridho Prasetya <ndiing.inc@gmail.com>
2024-05-17 03:57:03 +0700

    save

--- src/dev/main/main.js
+++ src/dev/main/main.js
@@ -9,125 +9,119 @@ class DevMain extends MDElement {
         super();
         this.list = [
             {
-                
                 label: "App Bar",
                 children: [
-                    {  label: "Bottom App Bar", routerLink: "/bottom-app-bar" },
-                    {  label: "Top App Bar", routerLink: "/top-app-bar" },
+                    { label: "Bottom App Bar", routerLink: "/bottom-app-bar" },
+                    { label: "Top App Bar", routerLink: "/top-app-bar" },
                 ],
             },
-            {  label: "Badge", routerLink: "/badge" },
+            { label: "Badge", routerLink: "/badge" },
             {
-                
                 label: "Button",
                 children: [
-                    {  label: "Button", routerLink: "/button" },
-                    {  label: "Fab", routerLink: "/fab" },
-                    {  label: "Extended Fab", routerLink: "/extended-fab" },
-                    {  label: "Icon Button", routerLink: "/icon-button" },
-                    {  label: "Segmented Button", routerLink: "/segmented-button" },
+                    { label: "Button", routerLink: "/button" },
+                    { label: "Fab", routerLink: "/fab" },
+                    { label: "Extended Fab", routerLink: "/extended-fab" },
+                    { label: "Icon Button", routerLink: "/icon-button" },
+                    { label: "Segmented Button", routerLink: "/segmented-button" },
                 ],
             },
-            {  label: "Card", routerLink: "/card" },
-            {  label: "Carousel", routerLink: "/carousel" },
-            {  label: "Checkbox", routerLink: "/checkbox" },
-            {  label: "Chip", routerLink: "/chip" },
-            {  label: "Date Picker", routerLink: "/date-picker" },
-            {  label: "Dialog", routerLink: "/dialog" },
-            {  label: "Divider", routerLink: "/divider" },
-            {  label: "List", routerLink: "/list" },
-            {  label: "Menu", routerLink: "/menu" },
+            { label: "Card", routerLink: "/card" },
+            { label: "Carousel", routerLink: "/carousel" },
+            { label: "Checkbox", routerLink: "/checkbox" },
+            { label: "Chip", routerLink: "/chip" },
+            { label: "Date Picker", routerLink: "/date-picker" },
+            { label: "Dialog", routerLink: "/dialog" },
+            { label: "Divider", routerLink: "/divider" },
+            { label: "List", routerLink: "/list" },
+            { label: "Menu", routerLink: "/menu" },
             {
-                
                 label: "Navigation",
                 children: [
-                    {  label: "Navigation Bar", routerLink: "/navigation-bar" },
-                    {  label: "Navigation Drawer", routerLink: "/navigation-drawer" },
-                    {  label: "Navigation Rail", routerLink: "/navigation-rail" },
+                    { label: "Navigation Bar", routerLink: "/navigation-bar" },
+                    { label: "Navigation Drawer", routerLink: "/navigation-drawer" },
+                    { label: "Navigation Rail", routerLink: "/navigation-rail" },
                 ],
             },
-            {  label: "Progress Indicator", routerLink: "/progress-indicator" },
-            {  label: "Radio Button", routerLink: "/radio-button" },
+            { label: "Progress Indicator", routerLink: "/progress-indicator" },
+            { label: "Radio Button", routerLink: "/radio-button" },
             {
-                
                 label: "Sheet",
                 children: [
-                    {  label: "Bottom Sheet", routerLink: "/bottom-sheet" },
-                    {  label: "Side Sheet", routerLink: "/side-sheet" },
+                    { label: "Bottom Sheet", routerLink: "/bottom-sheet" },
+                    { label: "Side Sheet", routerLink: "/side-sheet" },
                 ],
             },
-            {  label: "Slider", routerLink: "/slider" },
-            {  label: "Snackbar", routerLink: "/snackbar" },
-            {  label: "Switch", routerLink: "/switch" },
-            {  label: "Tab", routerLink: "/tab" },
-            {  label: "Text Field", routerLink: "/text-field" },
-            {  label: "Time Picker", routerLink: "/time-picker" },
-            {  label: "Tooltip", routerLink: "/tooltip" },
+            { label: "Slider", routerLink: "/slider" },
+            { label: "Snackbar", routerLink: "/snackbar" },
+            { label: "Switch", routerLink: "/switch" },
+            { label: "Tab", routerLink: "/tab" },
+            { label: "Text Field", routerLink: "/text-field" },
+            { label: "Time Picker", routerLink: "/time-picker" },
+            { label: "Tooltip", routerLink: "/tooltip" },
             {
-                
                 label: "Foundations",
                 children: [
-                    {  label: "Gesture", routerLink: "/gesture" },
-                    {  label: "Localization", routerLink: "/localization" },
-                    {  label: "Marker", routerLink: "/marker" },
-                    {  label: "Observer", routerLink: "/observer" },
-                    {  label: "Polyfill", routerLink: "/polyfill" },
-                    {  label: "Popper", routerLink: "/popper" },
-                    {  label: "Progress", routerLink: "/progress" },
-                    {  label: "Ripple", routerLink: "/ripple" },
-                    {  label: "Router", routerLink: "/router" },
-                    {  label: "Store", routerLink: "/store" },
-                    {  label: "Scrolling", routerLink: "/scrolling" },
-                    {  label: "Element", routerLink: "/element" },
+                    { label: "Gesture", routerLink: "/gesture" },
+                    { label: "Localization", routerLink: "/localization" },
+                    { label: "Marker", routerLink: "/marker" },
+                    { label: "Observer", routerLink: "/observer" },
+                    { label: "Polyfill", routerLink: "/polyfill" },
+                    { label: "Popper", routerLink: "/popper" },
+                    { label: "Progress", routerLink: "/progress" },
+                    { label: "Ripple", routerLink: "/ripple" },
+                    { label: "Router", routerLink: "/router" },
+                    { label: "Store", routerLink: "/store" },
+                    { label: "Scrolling", routerLink: "/scrolling" },
+                    { label: "Element", routerLink: "/element" },
                 ],
             },
             {
-                
                 label: "Components",
                 children: [
-                    {  label: "Color Field", routerLink: "/color-field" },
-                    {  label: "Color Picker", routerLink: "/color-picker" },
-                    {  label: "Data Table", routerLink: "/data-table" },
-                    {  label: "Date Field", routerLink: "/date-field" },
-                    {  label: "Datetime Field", routerLink: "/datetime-field" },
-                    {  label: "Datetime Picker", routerLink: "/datetime-picker" },
-                    {  label: "Email Field", routerLink: "/email-field" },
-                    {  label: "Emoji", routerLink: "/emoji" },
-                    {  label: "File Field", routerLink: "/file-field" },
-                    {  label: "Form", routerLink: "/form" },
-                    {  label: "Icon", routerLink: "/icon" },
-                    {  label: "Image", routerLink: "/image" },
-                    {  label: "Layout", routerLink: "/layout" },
-                    {  label: "Month Field", routerLink: "/month-field" },
-                    {  label: "Month Picker", routerLink: "/month-picker" },
-                    {  label: "Number Field", routerLink: "/number-field" },
-                    {  label: "Pagination", routerLink: "/pagination" },
-                    {  label: "Password Field", routerLink: "/password-field" },
-                    {  label: "Pane", routerLink: "/pane" },
-                    {  label: "Search Field", routerLink: "/search-field" },
-                    {  label: "Select Field", routerLink: "/select-field" },
-                    {  label: "Tel Field", routerLink: "/tel-field" },
-                    {  label: "Textarea Field", routerLink: "/textarea-field" },
-                    {  label: "Time Field", routerLink: "/time-field" },
-                    {  label: "Url Field", routerLink: "/url-field" },
-                    {  label: "Week Field", routerLink: "/week-field" },
-                    {  label: "Week Picker", routerLink: "/week-picker" },
-                    {  label: "Nested List", routerLink: "/nested-list" },
+                    { label: "Color Field", routerLink: "/color-field" },
+                    { label: "Color Picker", routerLink: "/color-picker" },
+                    { label: "Data Table", routerLink: "/data-table" },
+                    { label: "Date Field", routerLink: "/date-field" },
+                    { label: "Datetime Field", routerLink: "/datetime-field" },
+                    { label: "Datetime Picker", routerLink: "/datetime-picker" },
+                    { label: "Email Field", routerLink: "/email-field" },
+                    { label: "Emoji", routerLink: "/emoji" },
+                    { label: "File Field", routerLink: "/file-field" },
+                    { label: "Form", routerLink: "/form" },
+                    { label: "Icon", routerLink: "/icon" },
+                    { label: "Image", routerLink: "/image" },
+                    { label: "Layout", routerLink: "/layout" },
+                    { label: "Month Field", routerLink: "/month-field" },
+                    { label: "Month Picker", routerLink: "/month-picker" },
+                    { label: "Number Field", routerLink: "/number-field" },
+                    { label: "Pagination", routerLink: "/pagination" },
+                    { label: "Password Field", routerLink: "/password-field" },
+                    { label: "Pane", routerLink: "/pane" },
+                    { label: "Search Field", routerLink: "/search-field" },
+                    { label: "Select Field", routerLink: "/select-field" },
+                    { label: "Tel Field", routerLink: "/tel-field" },
+                    { label: "Textarea Field", routerLink: "/textarea-field" },
+                    { label: "Time Field", routerLink: "/time-field" },
+                    { label: "Url Field", routerLink: "/url-field" },
+                    { label: "Week Field", routerLink: "/week-field" },
+                    { label: "Week Picker", routerLink: "/week-picker" },
+                    { label: "Nested List", routerLink: "/nested-list" },
                 ],
             },
         ];
 
-        // this.list=this.list.filter(item=>!item.children?.length)
+        this.select(this.list);
+    }
 
-        function setSelected(list) {
-            list.forEach((item) => {
-                item.selected = item.routerLink == Router.path;
-                if (item.children?.length) {
-                    setSelected(item.children);
-                }
-            });
-        }
-        setSelected(this.list);
+    select(list) {
+        list.forEach((item) => {
+            item.selected = item.routerLink == Router.path;
+
+            if (item.children?.length) {
+                this.select(item.children);
+            }
+        });
     }
 
     render() {
@@ -138,7 +132,6 @@ class DevMain extends MDElement {
                 <md-navigation-drawer
                     id="navigation-drawer"
                     .list="${this.list}"
-                    .uiList="${"card"}"
                     @onListItemSelected="${this.handleDevMainListItemSelected}"
                 ></md-navigation-drawer>
 
