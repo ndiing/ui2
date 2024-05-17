f7dfee30f2416f7a0d2c4db827c4e50c2f28e934
Ridho Prasetya <ndiing.inc@gmail.com>
2024-05-17 18:32:15 +0700

    save

--- src/dev/form/form.js
+++ src/dev/form/form.js
@@ -3,77 +3,104 @@ import { html } from "lit";
 import { msg } from "@lit/localize";
 
 class DevForm extends MDElement {
-    constructor(){
-        super()
-        this.data={
-            "email": "",
-            "password": "",
-            "text": "",
-            "url": "",
-            "search": "",
-            "number": "",
-            "tel": "",
-            "date": "2024-05-10",
-            "datetime": "2024-05-10T04:13",
-            "month": "2024-05",
-            "time": "04:13",
-            "week": "2024-W19",
-            "color": "#36693d",
-            "select": "",
-            "textarea": "",
-            "slider": "50"
-        }
-    }
+
     render() {
         // prettier-ignore
         return html`
-            <md-form
-                @onFormNativeSubmit="${this.handleFormNativeSubmit}"
-            >
+            <md-form action="" enctype="" method="">
                 <div class="md-layout-column" style="margin:24px;">
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-email-field ui="outlined" label="email" name="email" .value="${this.data.email}" .defaultValue="${this.data.email}"></md-email-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-password-field ui="outlined" label="password" name="password" .value="${this.data.password}" .defaultValue="${this.data.password}"></md-password-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-text-field ui="outlined" label="text" name="text" .value="${this.data.text}" .defaultValue="${this.data.text}"></md-text-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-url-field ui="outlined" label="url" name="url" .value="${this.data.url}" .defaultValue="${this.data.url}"></md-url-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-search-field ui="outlined" label="search" name="search" .value="${this.data.search}" .defaultValue="${this.data.search}"></md-search-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-number-field ui="outlined" label="number" name="number" .value="${this.data.number}" .defaultValue="${this.data.number}"></md-number-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-tel-field ui="outlined" label="tel" name="tel" .value="${this.data.tel}" .defaultValue="${this.data.tel}"></md-tel-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-date-field ui="outlined" label="date" name="date" .value="${this.data.date}" .defaultValue="${this.data.date}"></md-date-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-datetime-field ui="outlined" label="datetime" name="datetime" .value="${this.data.datetime}" .defaultValue="${this.data.datetime}"></md-datetime-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-month-field ui="outlined" label="month" name="month" .value="${this.data.month}" .defaultValue="${this.data.month}"></md-month-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-time-field ui="outlined" label="time" name="time" .value="${this.data.time}" .defaultValue="${this.data.time}"></md-time-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-week-field ui="outlined" label="week" name="week" .value="${this.data.week}" .defaultValue="${this.data.week}"></md-week-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-color-field ui="outlined" label="color" name="color" .value="${this.data.color}" .defaultValue="${this.data.color}"></md-color-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-file-field ui="outlined" label="file" name="file" .value="${this.data.file}" .defaultValue="${this.data.file}"></md-file-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-select-field ui="outlined" label="select" name="select" .value="${this.data.select}" .defaultValue="${this.data.select}"></md-select-field></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-textarea-field ui="outlined" label="textarea" name="textarea" .value="${this.data.textarea}" .defaultValue="${this.data.textarea}"></md-textarea-field></div>
-
-                    <div class="md-layout-column__item md-layout-column__item--expanded12 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-slider name="slider"></md-slider></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded12 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-checkbox name="checkbox"></md-checkbox></div>
-                    <div class="md-layout-column__item md-layout-column__item--expanded12 md-layout-column__item--medium4 md-layout-column__item--compact4"><md-radio-button name="radio-button"></md-radio-button></div>
-
-                    <div class="md-layout-column__item md-layout-column__item--expanded12 md-layout-column__item--medium4 md-layout-column__item--compact4">
-                        <md-button type="reset" label="Reset" ui="filled-tonal"></md-button>
-                        <md-button type="submit" label="Submit" ui="filled"></md-button>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-color-field label="Color" name="" placeholder="" required ui="outlined" text=""></md-color-field>
                     </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-date-field label="Date" name="" placeholder="" required ui="outlined" text=""></md-date-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-datetime-field label="Datetime" name="" placeholder="" required ui="outlined" text=""></md-datetime-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-email-field label="Email" name="" placeholder="" required ui="outlined" text=""></md-email-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-file-field label="File" name="" placeholder="" required ui="outlined" text=""></md-file-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-month-field label="Month" name="" placeholder="" required ui="outlined" text=""></md-month-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-number-field label="Number" name="" placeholder="" required ui="outlined" text=""></md-number-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-password-field label="Password" type="" name="" placeholder="" required ui="outlined" text=""></md-password-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-search-field label="Search" name="" placeholder="" required ui="outlined" text=""></md-search-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-select-field label="Select" name="" placeholder="" required ui="outlined" text=""></md-select-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-tel-field label="Tel" name="" placeholder="" required ui="outlined" text=""></md-tel-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-text-field label="Text" name="" placeholder="" required ui="outlined" text=""></md-text-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-textarea-field label="Textarea" name="" placeholder="" required ui="outlined" text=""></md-textarea-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-time-field label="Time" name="" placeholder="" required ui="outlined" text=""></md-time-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-url-field label="Url" name="" placeholder="" required ui="outlined" text=""></md-url-field>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-week-field label="Week" name="" placeholder="" required ui="outlined" text=""></md-week-field>
+                    </div>
+
+                    <div class="md-layout-column__item--expanded12 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-slider></md-slider>
+                    </div>
+
+                    <div class="md-layout-column__item--expanded12 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-checkbox></md-checkbox>
+                        <md-checkbox indeterminate></md-checkbox>
+                        <md-checkbox checked defaultChecked></md-checkbox>
+                    </div>
+                    <div class="md-layout-column__item--expanded12 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-radio-button></md-radio-button>
+                        <md-radio-button checked defaultChecked></md-radio-button>
+                    </div>
+                    <div class="md-layout-column__item--expanded12 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-switch></md-switch>
+                        <md-switch checked defaultChecked></md-switch>
+                    </div>
+
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-color-picker></md-color-picker>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-date-picker></md-date-picker>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-datetime-picker></md-datetime-picker>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-month-picker></md-month-picker>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-time-picker></md-time-picker>
+                    </div>
+                    <div class="md-layout-column__item--expanded3 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                        <md-week-picker></md-week-picker>
+                    </div>
+
                 </div>
             </md-form>
+
         `;
     }
-    handleFormNativeSubmit(event){
 
-        // console.log(Object.fromEntries(new FormData(event.detail.target).entries()))   
-
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            body: JSON.stringify(Object.fromEntries(new FormData(event.detail.target).entries())),
-            headers: {
-              'Content-type': 'application/json; charset=UTF-8',
-            },
-          })
-            .then((response) => response.json())
-            .then((json) => console.log(json));
-    }
 }
 
 customElements.define("dev-form", DevForm);
--- src/dev/index.js
+++ src/dev/index.js
@@ -5,44 +5,44 @@ const routes = [
         path: "",
         load: () => import("./main/main.js").then((m) => m.default),
         children: [
-            /* group */
+
             { path: "polyfill", load: () => import("./polyfill/polyfill.js").then((m) => m.default) },
             { path: "element", load: () => import("./element/element.js").then((m) => m.default) },
             { path: "localization", load: () => import("./localization/localization.js").then((m) => m.default) },
-            /* group */
+
             { path: "marker", load: () => import("./marker/marker.js").then((m) => m.default) },
             { path: "observer", load: () => import("./observer/observer.js").then((m) => m.default) },
             { path: "progress", load: () => import("./progress/progress.js").then((m) => m.default) },
             { path: "router", load: () => import("./router/router.js").then((m) => m.default) },
             { path: "store", load: () => import("./store/store.js").then((m) => m.default) },
-            /* group */
+
             { path: "gesture", load: () => import("./gesture/gesture.js").then((m) => m.default) },
             { path: "popper", load: () => import("./popper/popper.js").then((m) => m.default) },
             { path: "ripple", load: () => import("./ripple/ripple.js").then((m) => m.default) },
             { path: "scrolling", load: () => import("./scrolling/scrolling.js").then((m) => m.default) },
-            /* group */
+
             { path: "button", load: () => import("./button/button.js").then((m) => m.default) },
             { path: "extended-fab", load: () => import("./extended-fab/extended-fab.js").then((m) => m.default) },
             { path: "fab", load: () => import("./fab/fab.js").then((m) => m.default) },
             { path: "segmented-button", load: () => import("./segmented-button/segmented-button.js").then((m) => m.default) },
             { path: "icon-button", load: () => import("./icon-button/icon-button.js").then((m) => m.default) },
-            /* group */
+
             { path: "emoji", load: () => import("./emoji/emoji.js").then((m) => m.default) },
             { path: "icon", load: () => import("./icon/icon.js").then((m) => m.default) },
             { path: "image", load: () => import("./image/image.js").then((m) => m.default) },
             { path: "badge", load: () => import("./badge/badge.js").then((m) => m.default) },
             { path: "layout", load: () => import("./layout/layout.js").then((m) => m.default) },
-            /* group */
+
             { path: "carousel", load: () => import("./carousel/carousel.js").then((m) => m.default) },
             { path: "chip", load: () => import("./chip/chip.js").then((m) => m.default) },
             { path: "divider", load: () => import("./divider/divider.js").then((m) => m.default) },
             { path: "progress-indicator", load: () => import("./progress-indicator/progress-indicator.js").then((m) => m.default) },
             { path: "snackbar", load: () => import("./snackbar/snackbar.js").then((m) => m.default) },
             { path: "tooltip", load: () => import("./tooltip/tooltip.js").then((m) => m.default) },
-            /* group */
+
             { path: "data-table", load: () => import("./data-table/data-table.js").then((m) => m.default) },
             { path: "pagination", load: () => import("./pagination/pagination.js").then((m) => m.default) },
-            /* group */
+
             { path: "pane", load: () => import("./pane/pane.js").then((m) => m.default) },
             { path: "bottom-app-bar", load: () => import("./bottom-app-bar/bottom-app-bar.js").then((m) => m.default) },
             { path: "bottom-sheet", load: () => import("./bottom-sheet/bottom-sheet.js").then((m) => m.default) },
@@ -50,23 +50,23 @@ const routes = [
             { path: "dialog", load: () => import("./dialog/dialog.js").then((m) => m.default) },
             { path: "side-sheet", load: () => import("./side-sheet/side-sheet.js").then((m) => m.default) },
             { path: "top-app-bar", load: () => import("./top-app-bar/top-app-bar.js").then((m) => m.default) },
-            /* group */
+
             { path: "list", load: () => import("./list/list.js").then((m) => m.default) },
             { path: "nested-list", load: () => import("./nested-list/nested-list.js").then((m) => m.default) },
-            /* group */
+
             { path: "menu", load: () => import("./menu/menu.js").then((m) => m.default) },
             { path: "navigation-bar", load: () => import("./navigation-bar/navigation-bar.js").then((m) => m.default) },
             { path: "navigation-drawer", load: () => import("./navigation-drawer/navigation-drawer.js").then((m) => m.default) },
             { path: "navigation-rail", load: () => import("./navigation-rail/navigation-rail.js").then((m) => m.default) },
             { path: "tab", load: () => import("./tab/tab.js").then((m) => m.default) },
-            /* group */
+
             { path: "form", load: () => import("./form/form.js").then((m) => m.default) },
-            /* group */
+
             { path: "checkbox", load: () => import("./checkbox/checkbox.js").then((m) => m.default) },
             { path: "radio-button", load: () => import("./radio-button/radio-button.js").then((m) => m.default) },
             { path: "switch", load: () => import("./switch/switch.js").then((m) => m.default) },
             { path: "slider", load: () => import("./slider/slider.js").then((m) => m.default) },
-            /* group */
+
             { path: "color-field", load: () => import("./color-field/color-field.js").then((m) => m.default) },
             { path: "date-field", load: () => import("./date-field/date-field.js").then((m) => m.default) },
             { path: "datetime-field", load: () => import("./datetime-field/datetime-field.js").then((m) => m.default) },
@@ -83,17 +83,17 @@ const routes = [
             { path: "time-field", load: () => import("./time-field/time-field.js").then((m) => m.default) },
             { path: "url-field", load: () => import("./url-field/url-field.js").then((m) => m.default) },
             { path: "week-field", load: () => import("./week-field/week-field.js").then((m) => m.default) },
-            /* group */
+
             { path: "color-picker", load: () => import("./color-picker/color-picker.js").then((m) => m.default) },
             { path: "date-picker", load: () => import("./date-picker/date-picker.js").then((m) => m.default) },
             { path: "datetime-picker", load: () => import("./datetime-picker/datetime-picker.js").then((m) => m.default) },
             { path: "month-picker", load: () => import("./month-picker/month-picker.js").then((m) => m.default) },
             { path: "time-picker", load: () => import("./time-picker/time-picker.js").then((m) => m.default) },
             { path: "week-picker", load: () => import("./week-picker/week-picker.js").then((m) => m.default) },
-            /* group */
+
             { path: "navigation", load: () => import("./navigation/navigation.js").then((m) => m.default) },
             { path: "picker", load: () => import("./picker/picker.js").then((m) => m.default) },
-            /* group */
+
             { path: "example", load: () => import("./example/example.js").then((m) => m.default) },
         ],
     },
