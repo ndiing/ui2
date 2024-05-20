79f1d163658ebfdb2de4f45fcc5e425f9ee7883f
Ridho Prasetya <ndiing.inc@gmail.com>
2024-05-20 22:21:00 +0700

    add chip

--- src/com/chip/chip.js
+++ src/com/chip/chip.js
@@ -1,10 +1,17 @@
 import { MDElement } from "../element/element";
-import { html } from "lit";
+import { html, nothing } from "lit";
 import { msg } from "@lit/localize";
+import { Ripple } from "../ripple/ripple";
 
 class MDChip extends MDElement {
     static get properties() {
-        return {};
+        return {
+            avatar:{type:String},
+            leadingIcon:{type:String},
+            label:{type:String},
+            trailingIcon:{type:String},
+            selected:{type:Boolean,reflect:true},
+        };
     }
 
     constructor() {
@@ -14,6 +21,14 @@ class MDChip extends MDElement {
     render() {
         // prettier-ignore
         return html`
+            ${this.selected?html`
+                <md-icon class="md-chip__indicator">check</md-icon>
+            `:html`
+                ${this.avatar?html`<md-image class="md-chip__avatar" .src="${this.avatar}" .ratio="${"1/1"}" .rounded="${true}"></md-image>`:nothing}
+                ${this.leadingIcon?html`<md-icon class="md-chip__icon">${this.leadingIcon}</md-icon>`:nothing}
+            `}
+            ${this.label?html`<div class="md-chip__label">${this.label}</div>`:nothing}
+            ${this.trailingIcon?html`<md-icon class="md-chip__icon">${this.trailingIcon}</md-icon>`:nothing}
         `
     }
 
@@ -21,11 +36,15 @@ class MDChip extends MDElement {
         super.connectedCallback();
         await this.updateComplete;
         this.classList.add("md-chip");
+
+        this.ripple=new Ripple(this,{})
     }
 
     disconnectedCallback() {
         super.disconnectedCallback();
         this.classList.remove("md-chip");
+
+        this.ripple.destroy()
     }
 
     updated(changedProperties) {}
--- src/dev/chip/chip.js
+++ src/dev/chip/chip.js
@@ -8,10 +8,59 @@ class DevChip extends MDElement {
         return html`
             <div class="md-layout-column" style="margin:24px;">
                 <div class="md-layout-column__item md-layout-column__item--expanded4 md-layout-column__item--medium4 md-layout-column__item--compact4">
-                    <md-chip></md-chip>
+                    <md-chip
+                        .label="${"Label"}"
+                    ></md-chip>
                 </div>
-                
-                
+                <div class="md-layout-column__item md-layout-column__item--expanded4 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                    <md-chip
+                        .leadingIcons="${"today"}"
+                        .label="${"Label"}"
+                    ></md-chip>
+                </div>
+                <div class="md-layout-column__item md-layout-column__item--expanded4 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                    <md-chip
+                        .leadingIcons="${"today"}"
+                        .label="${"Label"}"
+                        .trailingIcon="${"close"}"
+                    ></md-chip>
+                </div>
+                <div class="md-layout-column__item md-layout-column__item--expanded4 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                    <md-chip
+                        .avatar="${"https://placehold.co/600x400"}"
+                        .label="${"Label"}"
+                        .trailingIcon="${"close"}"
+                    ></md-chip>
+                </div>
+                <div class="md-layout-column__item md-layout-column__item--expanded4 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                    <md-chip
+                        .selected="${true}"
+                        .label="${"Label"}"
+                    ></md-chip>
+                </div>
+                <div class="md-layout-column__item md-layout-column__item--expanded4 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                    <md-chip
+                        .selected="${true}"
+                        .leadingIcons="${"today"}"
+                        .label="${"Label"}"
+                    ></md-chip>
+                </div>
+                <div class="md-layout-column__item md-layout-column__item--expanded4 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                    <md-chip
+                        .selected="${true}"
+                        .leadingIcons="${"today"}"
+                        .label="${"Label"}"
+                        .trailingIcon="${"close"}"
+                    ></md-chip>
+                </div>
+                <div class="md-layout-column__item md-layout-column__item--expanded4 md-layout-column__item--medium4 md-layout-column__item--compact4">
+                    <md-chip
+                        .selected="${true}"
+                        .avatar="${"https://placehold.co/600x400"}"
+                        .label="${"Label"}"
+                        .trailingIcon="${"close"}"
+                    ></md-chip>
+                </div>
             </div>
         `;
     }
