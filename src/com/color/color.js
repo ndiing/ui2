1596517a33cc67abc2c37e36b37f820515287d5c
Ridho Prasetya <ndiing.inc@gmail.com>
2024-06-02 05:32:29 +0700

    https://github.com/material-foundation/material-color-utilities/issues/98

--- src/com/color/color.js
+++ src/com/color/color.js
@@ -1,4 +1,4 @@
-import { Hct, applyTheme, argbFromHex, themeFromSourceColor } from "@material/material-color-utilities";
+import { Hct, applyTheme, argbFromHex, themeFromSourceColor, hexFromArgb } from "@material/material-color-utilities";
 
 class MDColorModule {
     constructor() {
@@ -33,6 +33,29 @@ class MDColorModule {
 
         const hex = document.querySelector('[name="theme-color"]').getAttribute("content");
         const theme = themeFromSourceColor(argbFromHex(hex), []);
+
+        // https://github.com/material-foundation/material-color-utilities/issues/98
+        Object.assign(theme.schemes.light.props, {
+            surfaceDim: theme.palettes.neutral.tone(87),
+            surfaceBright: theme.palettes.neutral.tone(98),
+            surfaceContainerLowest: theme.palettes.neutral.tone(100),
+            surfaceContainerLow: theme.palettes.neutral.tone(96),
+            surfaceContainer: theme.palettes.neutral.tone(94),
+            surfaceContainerHigh: theme.palettes.neutral.tone(92),
+            surfaceContainerHighest: theme.palettes.neutral.tone(90),
+            surfaceTintColor: theme.schemes.light.primary,
+        });
+        Object.assign(theme.schemes.dark.props, {
+            surfaceDim: theme.palettes.neutral.tone(6),
+            surfaceBright: theme.palettes.neutral.tone(24),
+            surfaceContainerLowest: theme.palettes.neutral.tone(4),
+            surfaceContainerLow: theme.palettes.neutral.tone(10),
+            surfaceContainer: theme.palettes.neutral.tone(12),
+            surfaceContainerHigh: theme.palettes.neutral.tone(17),
+            surfaceContainerHighest: theme.palettes.neutral.tone(22),
+            surfaceTintColor: theme.schemes.dark.primary,
+        });
+
         applyTheme(theme, {
             target: document.body,
             dark: this.item.name == "dark",
