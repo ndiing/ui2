e3b57a5b90001308b4e58710959ed7afe29eb1ad
Ridho Prasetya <ndiing.inc@gmail.com>
2024-06-08 20:48:45 +0700

    update ripple

--- src/com/ripple/ripple.js
+++ src/com/ripple/ripple.js
@@ -2,15 +2,18 @@ class MDRippleModule {
     constructor(host, options) {
         this.host = host;
         this.options = {
-            button: this.host,
+            container: null,
+            button: null,
+
             containment: true,
-            size: undefined,
+            size: null,
             centered: false,
             fadeout: false,
             inverse: false,
+
             ...options,
         };
-        
+
         this.handleRippleButtonPointerenter = this.handleRippleButtonPointerenter.bind(this);
         this.handleRippleButtonPointerleave = this.handleRippleButtonPointerleave.bind(this);
         this.handleRippleButtonPointerdown = this.handleRippleButtonPointerdown.bind(this);
@@ -23,62 +26,65 @@ class MDRippleModule {
     }
 
     init() {
-        this.host.classList.add("md-ripple");
+        this.container = this.options.container||this.host;
+        this.button = this.options.button||this.host;
+
+        this.container.classList.add("md-ripple");
 
         if (this.options.containment) {
-            this.host.classList.add("md-ripple--containment");
+            this.container.classList.add("md-ripple--containment");
         } else {
-            this.host.classList.remove("md-ripple--containment");
+            this.container.classList.remove("md-ripple--containment");
         }
 
         if (this.options.fadeout) {
-            this.host.classList.add("md-ripple--fadeout");
+            this.container.classList.add("md-ripple--fadeout");
         } else {
-            this.host.classList.remove("md-ripple--fadeout");
+            this.container.classList.remove("md-ripple--fadeout");
         }
 
         if (this.options.inverse) {
-            this.host.classList.add("md-ripple--inverse");
+            this.container.classList.add("md-ripple--inverse");
         } else {
-            this.host.classList.remove("md-ripple--inverse");
+            this.container.classList.remove("md-ripple--inverse");
         }
 
         if (!this.options.size) {
-            const { width, height } = this.host.getBoundingClientRect();
-            
+            const { width, height } = this.container.getBoundingClientRect();
+
             this.options.size = (Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2)) / width) * 100;
         }
 
-        this.host.style.setProperty("--md-ripple-size", this.options.size + "%");
+        this.container.style.setProperty("--md-ripple-size", this.options.size + "%");
 
-        this.options.button.classList.add("md-ripple--button");
-        this.options.button.setAttribute("tabIndex", "0");
-        
-        this.options.button.addEventListener("pointerenter", this.handleRippleButtonPointerenter);
-        this.options.button.addEventListener("pointerleave", this.handleRippleButtonPointerleave);
-        this.options.button.addEventListener("pointerdown", this.handleRippleButtonPointerdown);
-        this.options.button.addEventListener("focus", this.handleRippleButtonFocus);
-        this.options.button.addEventListener("blur", this.handleRippleButtonBlur);
-        this.options.button.addEventListener("animationend", this.handleRippleButtonAnimationend);
+        this.button.classList.add("md-ripple--button");
+        this.button.setAttribute("tabIndex", "0");
+
+        this.button.addEventListener("pointerenter", this.handleRippleButtonPointerenter);
+        this.button.addEventListener("pointerleave", this.handleRippleButtonPointerleave);
+        this.button.addEventListener("pointerdown", this.handleRippleButtonPointerdown);
+        this.button.addEventListener("focus", this.handleRippleButtonFocus);
+        this.button.addEventListener("blur", this.handleRippleButtonBlur);
+        this.button.addEventListener("animationend", this.handleRippleButtonAnimationend);
     }
 
     handleRippleButtonPointerenter(event) {
-        this.host.classList.add("md-ripple--hover");
+        this.container.classList.add("md-ripple--hover");
     }
 
     handleRippleButtonPointerleave(event) {
-        this.host.classList.remove("md-ripple--hover");
+        this.container.classList.remove("md-ripple--hover");
     }
 
     handleRippleButtonPointerdown(event) {
         window.addEventListener("pointerup", this.handleRippleButtonPointerup);
 
-        this.host.classList.add("md-ripple--pressed");
+        this.container.classList.add("md-ripple--pressed");
 
-        this.host.style.setProperty("--md-ripple-animation", "none");
-        this.host.style.setProperty("--md-ripple-animation-fadeout", "none");
+        this.container.style.setProperty("--md-ripple-animation", "none");
+        this.container.style.setProperty("--md-ripple-animation-fadeout", "none");
 
-        const { left: _left, top: _top, width, height } = this.host.getBoundingClientRect();
+        const { left: _left, top: _top, width, height } = this.container.getBoundingClientRect();
 
         if (!this.options.centered) {
             const { clientX, clientY } = event;
@@ -89,18 +95,18 @@ class MDRippleModule {
             const x = (0.5 - left) * (100 / size);
             const y = (0.5 - top) * ((100 / size) * (height / width));
 
-            this.host.style.setProperty("--md-ripple-left", left * 100 + "%");
-            this.host.style.setProperty("--md-ripple-top", top * 100 + "%");
-            this.host.style.setProperty("--md-ripple-x", x * 100 + "%");
-            this.host.style.setProperty("--md-ripple-y", y * 100 + "%");
+            this.container.style.setProperty("--md-ripple-left", left * 100 + "%");
+            this.container.style.setProperty("--md-ripple-top", top * 100 + "%");
+            this.container.style.setProperty("--md-ripple-x", x * 100 + "%");
+            this.container.style.setProperty("--md-ripple-y", y * 100 + "%");
         }
 
-        this.host.style.setProperty("--md-ripple-animation", "md-ripple");
-        this.host.style.setProperty("--md-ripple-animation-fadeout", "md-ripple-fadeout");
+        this.container.style.setProperty("--md-ripple-animation", "md-ripple");
+        this.container.style.setProperty("--md-ripple-animation-fadeout", "md-ripple-fadeout");
     }
 
     handleRippleButtonPointerup(event) {
-        this.host.classList.remove("md-ripple--pressed");
+        this.container.classList.remove("md-ripple--pressed");
 
         window.removeEventListener("pointerup", this.handleRippleButtonPointerup);
     }
@@ -108,39 +114,39 @@ class MDRippleModule {
     handleRippleButtonAnimationend(event) {
         if (this.options.fadeout) {
             if (event.animationName == "md-ripple-fadeout") {
-                this.host.style.removeProperty("--md-ripple-animation");
-                this.host.style.removeProperty("--md-ripple-animation-fadeout");
+                this.container.style.removeProperty("--md-ripple-animation");
+                this.container.style.removeProperty("--md-ripple-animation-fadeout");
             }
         } else {
-            this.host.style.removeProperty("--md-ripple-animation");
+            this.container.style.removeProperty("--md-ripple-animation");
         }
     }
 
     handleRippleButtonFocus(event) {
-        this.host.classList.add("md-ripple--focused");
+        this.container.classList.add("md-ripple--focused");
     }
 
     handleRippleButtonBlur(event) {
-        this.host.classList.remove("md-ripple--focused");
+        this.container.classList.remove("md-ripple--focused");
     }
 
     destroy() {
-        this.host.classList.remove("md-ripple");
-        this.host.classList.remove("md-ripple--containment");
-        this.host.classList.remove("md-ripple--fadeout");
-        this.host.classList.remove("md-ripple--inverse");
+        this.container.classList.remove("md-ripple");
+        this.container.classList.remove("md-ripple--containment");
+        this.container.classList.remove("md-ripple--fadeout");
+        this.container.classList.remove("md-ripple--inverse");
 
-        this.host.style.removeProperty("--md-ripple-size");
+        this.container.style.removeProperty("--md-ripple-size");
 
-        this.options.button.classList.remove("md-ripple--button");
-        this.options.button.removeAttribute("tabIndex");
+        this.button.classList.remove("md-ripple--button");
+        this.button.removeAttribute("tabIndex");
 
-        this.options.button.removeEventListener("pointerenter", this.handleRippleButtonPointerenter);
-        this.options.button.removeEventListener("pointerleave", this.handleRippleButtonPointerleave);
-        this.options.button.removeEventListener("pointerdown", this.handleRippleButtonPointerdown);
-        this.options.button.removeEventListener("focus", this.handleRippleButtonFocus);
-        this.options.button.removeEventListener("blur", this.handleRippleButtonBlur);
-        this.options.button.removeEventListener("animationend", this.handleRippleButtonAnimationend);
+        this.button.removeEventListener("pointerenter", this.handleRippleButtonPointerenter);
+        this.button.removeEventListener("pointerleave", this.handleRippleButtonPointerleave);
+        this.button.removeEventListener("pointerdown", this.handleRippleButtonPointerdown);
+        this.button.removeEventListener("focus", this.handleRippleButtonFocus);
+        this.button.removeEventListener("blur", this.handleRippleButtonBlur);
+        this.button.removeEventListener("animationend", this.handleRippleButtonAnimationend);
     }
 }
 export { MDRippleModule };
