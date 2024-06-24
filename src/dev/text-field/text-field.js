f883b9270bc1cc84e473c5a142efaaafe4df1e77
Ridho Prasetya <ndiing.inc@gmail.com>
2024-06-24 16:43:12 +0700

    temp update

--- src/dev/text-field/text-field.js
+++ src/dev/text-field/text-field.js
@@ -4,12 +4,72 @@ import { MDComponent } from "../../material/component/component.js";
 class DevTextFieldComponent extends MDComponent {
     render() {
         return html`
-            <div class="md-layout-column">
-                <div class="md-layout-column__item md-layout-column__item--expanded12 md-layout-column__item--medium8 md-layout-column__item--compact4">
-                    <md-text-field></md-text-field>
+            <md-form
+                @onFormNativeReset="${this.handleFormNativeReset}"
+                @onFormNativeSubmit="${this.handleFormNativeSubmit}"
+            >
+                <div class="md-layout-column">
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field required></md-text-field>
+                    </div>
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field placeholder="Placeholder" text="Text"></md-text-field>
+                    </div>
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field placeholder="Placeholder" value="Value" prefix="Rp"></md-text-field>
+                    </div>
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field prefix="Rp" suffix=",00"></md-text-field>
+                    </div>
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field icon="image"></md-text-field>
+                    </div>
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field required icon="image" actions='[{"icon":"image"}]'></md-text-field>
+                    </div>
+
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field required label="Label"></md-text-field>
+                    </div>
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field placeholder="Placeholder" label="Label" text="Text"></md-text-field>
+                    </div>
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field placeholder="Placeholder" value="Value" label="Label" prefix="Rp"></md-text-field>
+                    </div>
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field label="Label" prefix="Rp" suffix=",00"></md-text-field>
+                    </div>
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field label="Label" icon="image"></md-text-field>
+                    </div>
+                    <div class="md-layout-column__item md-layout-column__item--expanded2 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-text-field required label="Label" icon="image" actions='[{"icon":"image"}]'></md-text-field>
+                    </div>
+
+                    <div class="md-layout-column__item md-layout-column__item--expanded12 md-layout-column__item--medium8 md-layout-column__item--compact4">
+                        <md-button
+                            label="Reset"
+                            type="reset"
+                            variant="outlined"
+                        ></md-button>
+                        <md-button
+                            label="Submit"
+                            type="submit"
+                            variant="filled"
+                        ></md-button>
+                    </div>
                 </div>
-            </div>
+            </md-form>
         `;
+    }
+
+    handleFormNativeReset(event) {
+        console.log(event);
+    }
+
+    handleFormNativeSubmit(event) {
+        console.log(JSON.stringify(event.detail.data, null, 4));
     }
 }
 
